dbd1a5fe94835f7529b9ee127dad46bb1067c9ea
ChanulPathirana <chaanpathi004@gmail.com>
2026-03-30 00:05:34 +0530

    Doctor UI Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
-import DoctorPortal from "./pages/DoctorPortal";
 import PharmacyPortal from "./pages/PharmacyPortal";
+import MediScriptDashboard from "./pages/MediScriptDashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/doctor" element={<DoctorPortal />} />
+        <Route path="/doctor" element={<MediScriptDashboard/>} />
         <Route path="/pharmacy/1" element={<PharmacyPortal pharmacyId={1} />} />
         <Route path="/pharmacy/2" element={<PharmacyPortal pharmacyId={2} />} />
         <Route path="*" element={<Navigate to="/" replace />} />
