a35c123c5aac01078f214cd9ba8de280410362e7
ChanulPathirana <chaanpathi004@gmail.com>
2026-03-30 00:39:45 +0530

    Writing Pad Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import PharmacyPortal from "./pages/PharmacyPortal";
 import MediScriptDashboard from "./pages/MediScriptDashboard";
+import WritingPad from "./pages/WritingPad";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/doctor" element={<MediScriptDashboard/>} />
         <Route path="/pharmacy/1" element={<PharmacyPortal pharmacyId={1} />} />
         <Route path="/pharmacy/2" element={<PharmacyPortal pharmacyId={2} />} />
+        <Route path="/writing-pad" element={<WritingPad />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
