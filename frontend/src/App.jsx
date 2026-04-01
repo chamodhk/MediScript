fdfdd4c8bcb4f90a53963168e813cad1fe089d2f
ChanulPathirana <chaanpathi004@gmail.com>
2026-04-01 19:19:36 +0530

    app.jsx fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MediScriptDashboard />} />
+        <Route path="/" element={<LoginPage />} />
         <Route
           path="/admin"
           element={
